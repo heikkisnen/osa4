b6ccecf93794eb41ee9956f0602ba3ede4ddb4ff
Heikkinen <teemu.heikkinen@cgi.com>
2018-09-25 11:42:35 +0300

    filut gittiin

---
+++ index.js
@@ -0,0 +1,56 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Blog = mongoose.model('Blog', {
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+module.exports = Blog
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const mongoUrl = 'mongodb://teme:teme69@ds113923.mlab.com:13923/blog'
+mongoose.connect(mongoUrl)
+
+app.get('/', (request, response) => {
+    response.send('<h1> Hello, World! </h1>')
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.get('/api/blogs/:id', (request, response) => {
+    Blog
+      .findById(request.params.id)
+      .then(blog => {
+          response.json(blog)
+      })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
